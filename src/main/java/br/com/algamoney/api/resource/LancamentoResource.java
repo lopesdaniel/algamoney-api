34cba1d3f7d7850214bf4feb01909ea621b6094f
Daniel Lopes <viana.daniel02@gmail.com>
2019-11-27 11:23:31 -0200

    Validando incosistencias ao cadastrar novo lançamento

--- src/main/java/br/com/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/br/com/algamoney/api/resource/LancamentoResource.java
@@ -2,13 +2,21 @@ package br.com.algamoney.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.algamoney.api.event.RecursoCriadoEvent;
 import br.com.algamoney.api.model.Lancamento;
 import br.com.algamoney.api.repository.LancamentoRepository;
 
@@ -18,6 +26,9 @@ public class LancamentoResource {
 
 	@Autowired
 	private LancamentoRepository lancamentoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Lancamento> listar(){
@@ -30,5 +41,36 @@ public class LancamentoResource {
 		
 		return lancamento == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(lancamento);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> criar(@RequestBody @Valid Lancamento lancamento, HttpServletResponse response){
+		Lancamento lancamentoSalvo = lancamentoRepository.save(lancamento);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
